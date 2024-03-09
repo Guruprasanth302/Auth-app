d85cb0ad35261e81548847b688b11aad47614221
Guruprasanth302 <pguru1218@gmail.com>
2024-03-09 19:49:43 +0530

    adding mongodb database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
+// password for mongo db cluster - GqxVjc8UfPMZzTl3   and username is - guruprasanthp2020csea
 const app = express()
-
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("sucessfull")
+})
 app.listen(3000,()=>{
     console.log("Server is running on the port 3000");
 })
